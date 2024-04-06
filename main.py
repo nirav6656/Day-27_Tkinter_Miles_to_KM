5e95bf740c77bc68646e939d6a057c1a9134bc0c
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-04-06 19:04:04 -0400

    Listbox, Spinbox, Scale and Radiobox Class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()
 
-window.minsize(width=400,height=400)
+window.minsize(width=400,height=600)
 window.title("Hello World")
 
 label = Label(text="Hello world")
@@ -18,5 +18,46 @@ button.pack()
 
 input = Entry(width=10)
 input.pack()
+
+large_box = Text(width=20,height=10)
+large_box.focus()
+large_box.pack()
+print(large_box.get("1.0", END))
+large_box.pack()
+
+# Spinbox
+spinbox = Spinbox(from_=0,to=5)
+spinbox.pack()
+
+# Scale
+scale = Scale(from_=0, to=10)
+scale.pack()
+
+
+def onclick():
+
+    print(chekedstate.get())
+
+chekedstate = IntVar()
+checkbutton = Checkbutton(text="Is on",variable=chekedstate,command=onclick)
+checkbutton.pack()
+
+# Radiobutton
+def onclick():
+    print(clicked_state.get())
+clicked_state = IntVar()
+
+radiobutton1 = Radiobutton(text="Option 1",value=1,variable=clicked_state,command=onclick)
+radiobutton2 = Radiobutton(text="Option 2",value=2,variable=clicked_state,command=onclick)
+radiobutton1.pack()
+radiobutton2.pack()
+
+# Listbox
+listbox = Listbox(height=5)
+
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.pack()
 
 window.mainloop()
